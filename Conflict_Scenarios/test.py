16a67e355492de7779a5e51052a2b69ebab8e740
Narendra Singh <bns360@live.com>
2024-05-25 21:01:51 -0400

    troubleshooting

--- Conflict_Scenarios/test.py
+++ Conflict_Scenarios/test.py
@@ -1,3 +1,15 @@
-x = range(2, 8, 1)
-for item in x:
-    print(item)
+import pandas as pd
+
+# Read Data From Archive
+print("Reading Data...")
+data = pd.read_csv('Conflict Scenarios Research.csv')
+# create documents list
+documents = data["Describe a past experience you've had that involved conflict with a family member, friend, or significant other. Be as detailed as you like."].tolist()
+print("Number of Documents: " + str(len(documents)))
+
+count = 0
+for item in documents:
+    if isinstance(item, str):
+        count = count + 1
+
+print(count)
