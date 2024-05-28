348b39e639676394c085479d88d0622e1c48d166
Spartanlasergun <spartanlasergun@outlook.com>
2024-05-28 16:52:47 -0400

    completed UMAP param check

--- Conflict_Scenarios/test.py
+++ Conflict_Scenarios/test.py
@@ -1,15 +1,1 @@
-import pandas as pd
-
-# Read Data From Archive
-print("Reading Data...")
-data = pd.read_csv('Conflict Scenarios Research.csv')
-# create documents list
-documents = data["Describe a past experience you've had that involved conflict with a family member, friend, or significant other. Be as detailed as you like."].tolist()
-print("Number of Documents: " + str(len(documents)))
-
-count = 0
-for item in documents:
-    if isinstance(item, str):
-        count = count + 1
-
-print(count)
+#test
