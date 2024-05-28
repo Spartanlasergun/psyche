a2d3da7c31175a09259d13ad70e8083ffbc9d43f
Narendra Singh <bns360@live.com>
2024-05-28 17:03:33 -0400

    extracted good scores

--- Conflict_Scenarios/test.py
+++ Conflict_Scenarios/test.py
@@ -1,1 +1,25 @@
-#test
+scores = open("scores.txt", "r", encoding='utf8')
+data = scores.read().splitlines()
+scores.close()
+
+params = []
+
+for item in data:
+    temp = item.split(",")
+    params.append(temp)
+
+good = []
+for parameter in params:
+    coherence = float(parameter[0])
+    if coherence >= 0.18:
+        good.append(parameter)
+
+extract = open("good_scores.txt", 'w', encoding='utf8')
+for parang in good:
+    temp = ""
+    for item in parang:
+        temp = temp + str(item) + ","
+    temp = temp + "\n"
+    extract.write(temp)
+extract.close()
+
