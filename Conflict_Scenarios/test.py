d888ed0fb5c894ae72f78a1f7f0cc59c1257dba0
Narendra Singh <bns360@live.com>
2024-05-30 03:20:39 -0400

    updated .gitignore

--- Conflict_Scenarios/test.py
+++ Conflict_Scenarios/test.py
@@ -1,17 +1,14 @@
-tpc= range(2, 6, 1)
-cs= range(10, 21, 1)
-nb= range(5, 26, 1)
-comp= [3]
-umap_metric=['cosine']
-hdb_metric=['euclidean']
+import pymongo
 
-# generate grid
-grid = []
-for a in tpc:
-    for b in cs:
-        for c in nb:
-            for d in comp:
-                for e in umap_metric:
-                    for f in hdb_metric:
-                        temp = [a, b, c, d, e, f]
-                        grid.append(temp)
+uri = "mongodb+srv://cluster0.cd4m7jc.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=Cluster0"
+client = pymongo.MongoClient(uri,
+                             tls=True,
+                             tlsCertificateKeyFile='Spartanlasergun-certificate.pem',
+                             server_api=pymongo.server_api.ServerApi(version="1", strict=True, deprecation_errors=True))
+
+
+db = client['watchtower']
+collection = db['coherence_parameters']
+doc_count = collection.count_documents({})
+print(doc_count)
+
